bcf44402d283a5dd8cbb6b7f8868eea8676dab1a
KenGakuru2 <kennedygakuru3@gmail.com>
2025-04-30 15:11:26 -0400

    chore: create src folder to organize project structure

---
+++ src/screens/AddScreen.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+const AddScreen = (): JSX.Element => {
+    return(
+        <View className="flex-1 items-center justify-center bg-[#34A853]">
+            <Text>Add Medicine</Text>
+        </View>
+    );
+};
+
+export default AddScreen;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+const HomeScreen = () : JSX.Element => {
+    return(
+        <View className=" flex-1 bg-[#29B6F6] items-center justify-center">
+            <Text className="text bold">Pillre</Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
