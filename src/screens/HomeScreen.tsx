33c8280cc943db4d99468551f7397a792d6ab9b4
KenGakuru2 <kennedygakuru3@gmail.com>
2025-05-01 08:21:48 -0400

    style(home): update background color

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,7 +2,7 @@ import {View, Text} from 'react-native';
 
 const HomeScreen = () : JSX.Element => {
     return(
-        <View className=" flex-1 bg-[#29B6F6] items-center justify-center">
+        <View className=" flex-1 bg-[#0E1828] items-center justify-center">
             <Text className="text bold">Pillre</Text>
         </View>
     );
