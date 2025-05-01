1ef9ff94240a1d7520a88091f04348636c28bc23
KenGakuru2 <kennedygakuru3@gmail.com>
2025-05-01 08:45:59 -0400

    feat(navigation): implement StackNavigator as root
    - Added StackNavigator as primary navigation container in App.tsx
    - Configured initial route stack:
      • Onboarding (initialRoute)
      • Login
      • Main (contains nested BottomTabs)
    - Updated navigation types in types/navigation.ts
    - Verified smooth transitions between screens

---
+++ src/navigation/StackNavigator.tsx
@@ -0,0 +1,19 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import OnboardingScreen from '~/screens/OnboardingScreen';
+import LoginScreen from "~/screens/LoginScreen";
+import BottomTabs from "./BottomTabs";
+import { RootStackParamList } from "~/types/navigation";
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const StackNavigator = () => {
+    return(
+        <Stack.Navigator initialRouteName="Onboarding" screenOptions={ {headerShown: false}}>
+            <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Tabs" component={BottomTabs} />
+        </Stack.Navigator>
+    );
+};
+
+export default StackNavigator;
