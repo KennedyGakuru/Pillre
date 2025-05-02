e26e128f57a9ff8af037856ecfa911ec17873ba3
KenGakuru2 <kennedygakuru3@gmail.com>
2025-05-02 11:32:56 -0400

    feat: implement root navigation container

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 
 import './global.css';
 import { NavigationContainer } from '@react-navigation/native';
+import { JSX } from 'react';
 import BottomTabs from '~/navigation/BottomTabs';
 import StackNavigator from '~/navigation/StackNavigator';
 
 const App = (): JSX.Element => {
-  return (
+  return (  
       <NavigationContainer>
       <StackNavigator/>
-      </NavigationContainer>
+      </NavigationContainer>   
   );
 };
 
