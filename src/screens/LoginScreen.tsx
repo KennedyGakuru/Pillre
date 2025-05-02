d58ebd865d211abc1f81270a9f99cf1a5eff2215
KenGakuru2 <kennedygakuru3@gmail.com>
2025-05-02 11:29:19 -0400

    feat: add theme support to LoginScreen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,9 +1,12 @@
 import {View, Text} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useTheme } from '~/theme/colorScheme';
 
 const LoginScreen = () => {
+    const {theme}= useTheme();
     return(
-        <View>
-            <Text> Login</Text>
+        <View className="flex-1">
+            <Text className= "text-[#29B6F6] text-2xl"> Welcome back! Glad to see you, Again!</Text>
         </View>
     );
 }
