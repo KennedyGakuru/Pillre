2bda65fef8636938d860d69d8e6e89c5bb23a065
KenGakuru2 <kennedygakuru3@gmail.com>
2025-05-08 16:27:43 -0400

    feat: add lodash

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,15 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
-// eslint-disable-next-line no-undef
+// Get the default Expo config
 const config = getDefaultConfig(__dirname);
 
+// Add the lodash alias
+config.resolver.extraNodeModules = {
+  lodash: require.resolve('lodash'),
+};
+
+// Apply NativeWind config (ensure styles are processed)
 module.exports = withNativeWind(config, { input: './global.css' });
+
+
