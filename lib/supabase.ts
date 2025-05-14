4f67969ff7c6eb3d0b46bfc94d715b579d7db19c
KenGakuru2 <kennedygakuru3@gmail.com>
2025-05-14 16:22:56 -0400

    chore: downgrade Supabase to v1

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,11 @@
-// lib/supabase.ts
+
 import { createClient } from '@supabase/supabase-js'
+
 
 const SUPABASE_URL = 'https://runvmjaypjsizprdchxj.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1bnZtamF5cGpzaXpwcmRjaHhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxNDA3MzcsImV4cCI6MjA2MjcxNjczN30.klaMxTgWZQDTfxNFN1Xh9EAgDXyJoK5yo2L4K4HqbXs'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+
+
